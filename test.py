a40ee107663994d8185263f80483fec266da595b
somesh7292 <47203286+somesh7292@users.noreply.github.com>
2024-03-30 22:40:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from app import *
 
 def test_Home():
     response = app.test_client().get('/')
-    assert b"abc" in response.data
+    assert b"Hello World" in response.data
     assert response.status_code == 200
